6be5448cc32fb2f09840893b44a9fb370379b803
Yah-oo5726 <arsalannimaamini@gmail.com>
2026-05-30 23:42:55 -0700

    added commands and login command

---
+++ commands.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	config "github.com/Yah-oo5726/blog-aggregator/internal/config"
+)
+
+type state struct {
+	configPtr *config.Config
+}
+
+type command struct {
+	name      string
+	arguments []string
+}
+
+type commands struct {
+	functions map[string]func(*state, command) error
+}
+
+func handlerLogin(s *state, cmd command) error {
+	if len(cmd.arguments) == 0 {
+		return errors.New("username is required")
+	}
+	s.configPtr.SetUser(cmd.arguments[0])
+	fmt.Println("user has been set.")
+	return nil
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	function, exists := c.functions[cmd.name]
+	if !exists {
+		return errors.New("Function doesn't exist")
+	}
+	err := function(s, command{name: cmd.name, arguments: cmd.arguments})
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	return nil
+}
+
+func (c *commands) register(name string, f func(*state, command) error) error {
+	c.functions[name] = f
+	return nil
+}
--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	config "github.com/Yah-oo5726/blog-aggregator/internal/config"
+	"os"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("not enough arguments passed in")
+		os.Exit(1)
+	}
+	arguments := os.Args[1:]
 	configFile := config.Read()
-	configFile.SetUser("Yah_oo5726")
-	fmt.Printf("Username: %s, database url: %s\n", configFile.CurrentUserName, configFile.DbURL)
+	program_state := state{configPtr: &configFile}
+	program_commands := commands{functions: make(map[string]func(*state, command) error)}
+	program_commands.register("login", handlerLogin)
+	err := program_commands.run(&program_state, command{name: arguments[0], arguments: arguments[1:]})
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
 }
